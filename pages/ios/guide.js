3f27fefed34cfd3e10d561d04d38ca40935027d5
Ben Lei <benlei@oursky.com>
2016-04-25 12:53:23 +0800

    Add sample menu data
    Ref. #7

--- pages/ios/guide.js
+++ pages/ios/guide.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import GuideTemplate from '../../components/GuideTemplate';
 
+import store from '../../store/ios';
+
 export default class extends Component {
   render() {
     return (
       <GuideTemplate
         sectionName="iOS SDK"
+        menu={store.guide.menu}
+        activeMenu="How to install SkyKit SDK in Xcode"
         title="How to install SkyKit SDK in Xcode"
         content={require("../../content/ios/getting-started.md")}
       />
