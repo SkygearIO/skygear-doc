3de10c2ee96bb2baaf3547ba6062f7763d436b11
akiroz <akiroz.vectis@gmail.com>
2016-08-01 17:16:26 +0800

    replace get started page #252

---
+++ pages/get-started.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import SkygearGetStarted from '../lib/skygear-getstarted';
+
+export default class extends Component {
+  render() {
+    return (<SkygearGetStarted />);
+  }
+}
