fee77d4a9c6d7aaced598935ee44bca93f53bb48
bensonby <bensonby@oursky.com>
2016-11-17 15:50:54 +0800

    Create coming soon page

---
+++ pages/comingSoon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ComingSoonPage = () => (
+  <div>
+    <h1>Coming Soon</h1>
+    <p>Thanks for using Skygear. Please check back later for new updates.</p>
+  </div>
+);
+
+export default ComingSoonPage;
