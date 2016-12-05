6968ffe3a08e1f92312bbeab84fc88809d3b9c98
bensonby <bensonby@oursky.com>
2016-12-05 23:57:36 +0800

    Add landing page quick start text
    refs #392

--- pages/home/QuickStart.js
+++ pages/home/QuickStart.js
@@ -1,5 +1,14 @@
-const QuickStart = function () {
-  return false;
-};
+import React from 'react';
+import './QuickStart.scss';
+
+const QuickStart = () => (
+  <div className="quick-start">
+    <h1>Documentation</h1>
+    <p className="quick-start-hint">Just getting started?</p>
+    <p>
+      <button className="quick-start-button">Quick Start</button>
+    </p>
+  </div>
+);
 
 export default QuickStart;
