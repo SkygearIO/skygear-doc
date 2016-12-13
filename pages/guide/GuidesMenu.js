e35ab62cd8fbeba7e9c5814c118a441535171891
bensonby <bensonby@oursky.com>
2016-12-13 18:23:44 +0800

    Add basic language links
    refs #394

--- pages/guide/GuidesMenu.js
+++ pages/guide/GuidesMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import LanguageLink from '../../components/LanguageLink/LanguageLink';
 import guideListConfig from '../guideList/config';
 
 import './GuidesMenu.scss';
@@ -17,6 +18,15 @@ const GuidesMenu = () => (
           {section.guides.map(guide => (
             <div key={guide.title} className="guides-menu-item">
               <p className="guide-name">{guide.title}</p>
+              <div className="language-links">
+                {guide.languages.map(language => (
+                  <LanguageLink
+                    key={language}
+                    language={language}
+                    url={`${guide.baseUrl}${language}/`}
+                  />
+                ))}
+              </div>
             </div>
           ))}
         </div>
