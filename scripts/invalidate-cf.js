a8d0d1d74d2c6f6a916ca7238629fd76d3e35ef8
bensonby <bensonby@oursky.com>
2017-01-03 15:36:45 +0800

    Invalidate cloudfront objects upon prod deployment
    refs #416

---
+++ scripts/invalidate-cf.js
@@ -0,0 +1,41 @@
+const Void = require('void');
+
+const requiedEnv = [
+  'AWS_ACCESS_KEY_ID',
+  'AWS_SECRET_ACCESS_KEY',
+  'DISTRIBUTION_ID',
+];
+
+requiedEnv.forEach(function(perEnv) {
+  if (!process.env[perEnv]) {
+    console.error(`Error: \$${perEnv} is not set`);
+    process.exit(1);
+  }
+});
+
+const invalidatingPaths = [
+  '/app.js',
+  '/index.html',
+];
+
+const maxPaths =
+  invalidatingPaths.length < 1000 ? invalidatingPaths.length : 1000;
+
+const vPaths = new Void({
+  paths: invalidatingPaths,
+  maxPaths,
+});
+
+setInterval(function() {
+  let hasWaitingJob = false;
+  vPaths.queue.forEach(function(perJob) {
+    if (perJob.status === 'paused') {
+      hasWaitingJob = true;
+    }
+  });
+
+  if (!hasWaitingJob) {
+    console.log('Exit without waiting for all jobs complete');
+    process.exit(0);
+  }
+}, 1000);
