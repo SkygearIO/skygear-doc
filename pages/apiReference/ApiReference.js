9252fb9e9eb16cae9d2b3c75343494ff4165b345
Ben Cheng <bencheng@oursky.com>
2017-01-10 19:56:30 +0800

    Fixed #410, Add Chat API reference

--- pages/apiReference/ApiReference.js
+++ pages/apiReference/ApiReference.js
@@ -28,6 +28,30 @@ const ApiReference = () => (
         <div className="right-arrow" />
       </div>
     </a>
+    <a href="http://cocoadocs.org/docsets/SKYKitChat/0.0.1/Classes/SKYChatExtension.html" target="_blank">
+      <div className="api-reference-item">
+        <h2 className="ios-sdk">
+          iOS SDK (Chat)
+        </h2>
+        <div className="right-arrow" />
+      </div>
+    </a>
+    <a href="https://docs.skygear.io/android/plugins/chat/reference/" target="_blank">
+      <div className="api-reference-item">
+        <h2 className="android-sdk">
+          Android SDK (Chat)
+        </h2>
+        <div className="right-arrow" />
+      </div>
+    </a>
+    <a href="https://doc.esdoc.org/github.com/skygeario/chat-SDK-JS/" target="_blank">
+      <div className="api-reference-item">
+        <h2 className="js-sdk">
+          JavaScript SDK (Chat)
+        </h2>
+        <div className="right-arrow" />
+      </div>
+    </a>
   </div>
 );
 
