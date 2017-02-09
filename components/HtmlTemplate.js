c851566f1887f635ee8eb2dbfc0d01c57c4a319f
mayyuen318 <mayyuen318@users.noreply.github.com>
2017-02-09 12:37:54 +0800

    Update meta description & social media og tags Add missing og image referred in new header

--- components/HtmlTemplate.js
+++ components/HtmlTemplate.js
@@ -12,7 +12,22 @@ const Html = (props) => {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, minimum-scale=1.0" />
         <title>{props.title}</title>
-        <meta name="description" content="" />
+        <meta
+          name="description"
+          content="Skygear Docs are guides, API reference and sample projects
+          to help developers get started with Skygear iOS, Android and Javascript SDK."
+        />
+        <meta property="og:url" content="https://docs.skygear.io/" />
+        <meta property="og:title" content="Documentations - Skygear" />
+        <meta
+          name="og:description"
+          content="Skygear Docs are guides, API reference and sample projects
+          to help developers get started with Skygear iOS, Android and Javascript SDK."
+        />
+        <meta property="og:image" content="https://docs.skygear.io/static/images/skygear-og-img.png" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@skygeario" />
+        <meta name="twitter:creator" content="@skygeario" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,400italic,500,500italic,700,700italic" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
