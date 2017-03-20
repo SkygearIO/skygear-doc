b002362d9c2d26b13e9327eb4f8eef7683b27a92
Ben Lei <benlei@oursky.com>
2017-03-20 18:34:05 +0800

    Connect guide search widget with algolia API

--- components/GuideSearchWidget/GuideSearchWidget.js
+++ components/GuideSearchWidget/GuideSearchWidget.js
@@ -1,5 +1,11 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import Autosuggest from 'react-autosuggest';
+import algoliasearch from 'algoliasearch';
+
+import GuideSearchItem from './GuideSearchItem';
+
+import { algoliaApplicationID, algoliaSearchApiKey } from '../../config';
 
 import './GuideSearchWidget.scss';
 
@@ -8,6 +14,8 @@ import './GuideSearchWidget.scss';
 const AutosuggestTheme = {
   container: 'guide-search-widget',
   input: 'search-input',
+  inputOpen: 'search-input-active',
+  inputFocused: 'search-input-active',
   suggestionsContainer: 'suggestion-container',
   suggestionsContainerOpen: 'suggestion-container-open',
   suggestionsList: 'suggestion-list',
@@ -15,17 +23,22 @@ const AutosuggestTheme = {
   suggestionHighlighted: 'suggestion-list-item-highlighted',
 };
 
-const MaxSuggestionCount = 4;
+const MaxSuggestionCount = 5;
 
 class GuideSearchWidget extends Component {
   constructor(props) {
     super(props);
 
     this.onChange = this.onChange.bind(this);
-    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
-    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
+    this.onSuggestionsFetchRequested
+      = _.debounce(this.onSuggestionsFetchRequested.bind(this), 300);
+    this.onSuggestionsClearRequested =
+      this.onSuggestionsClearRequested.bind(this);
     this.getSuggestionValue = this.getSuggestionValue.bind(this);
     this.renderSuggestion = this.renderSuggestion.bind(this);
+
+    const algoliaClient = algoliasearch(algoliaApplicationID, algoliaSearchApiKey);
+    this.algoliaIndex = algoliaClient.initIndex('guides');
 
     this.state = {
       value: '',
@@ -40,19 +53,26 @@ class GuideSearchWidget extends Component {
   }
 
   onSuggestionsFetchRequested({ value }) {
-    // FIXME: Replace it with the real algoria search API
     if (value.length === 0) {
+      this.setState({
+        suggestions: [],
+      });
       return;
     }
 
-    setTimeout(() => {
-      const { suggestions } = this.state;
-      suggestions.push({ value });
+    this.algoliaIndex.search(
+      value,
+      { hitsPerPage: MaxSuggestionCount },
+      (err, guides) => {
+        if (err) {
+          console.error('Failed to search', err); // eslint-disable-line
+          return;
+        }
 
-      this.setState({
-        suggestions: suggestions.slice(-MaxSuggestionCount),
+        this.setState({
+          suggestions: guides.hits,
+        });
       });
-    }, 300);
   }
 
   onSuggestionsClearRequested() {
@@ -61,12 +81,12 @@ class GuideSearchWidget extends Component {
     });
   }
 
-  getSuggestionValue({ value }) {
-    return value;
+  getSuggestionValue(suggestion) {
+    return suggestion.title;
   }
 
   renderSuggestion(suggestion) {
-    return <span>{suggestion.value}</span>;
+    return <GuideSearchItem item={suggestion} />;
   }
 
   render() {
