9ca113b1c12b075c88de4f66a2a93875c3605e06
Ben Lei <benlei@oursky.com>
2017-05-04 16:29:14 +0800

    Merge Banner and Header component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,22 +1,27 @@
 import React, { PropTypes } from 'react';
-import './Header.scss';
 import { Link } from 'react-router';
 
 import logo from '../../static/docs-logo.png';
 
+import './Header.scss';
+
 const Header = (props) => (
-  <div className="header">
-    <Link to="/">
-      <img src={logo} className="logo" alt="Skygear Documentation" />
-    </Link>
-    <nav>
-      {props.children}
-    </nav>
+  <div className="header-banner">
+    <div className="header-banner-background">
+      <div className="header">
+        <Link to="/" className="logo-anchor" />
+        <nav className="header-nav">{props.children}</nav>
+      </div>
+    </div>
   </div>
 );
 
 Header.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.array
+};
+
+Header.defaultProps = {
+  children: []
 };
 
 export default Header;
