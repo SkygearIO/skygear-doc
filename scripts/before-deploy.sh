f7cea0191bba30ea1595d62eb034264f119ccece
Ben Lei <benlei@oursky.com>
2017-07-05 15:13:09 +0800

    Update before-deploy script not removing js api reference

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -5,12 +5,20 @@ if [[ "$TRAVIS_BRANCH" == "$PRODUCTION_BRANCH" ]]; then
   mv build/production-robots.txt build/robots.txt
   echo "Done"
   echo "Removing existing files on production..."
-  aws s3 rm s3://$PRODUCTION_BUCKET --recursive --exclude "android/*" --exclude "ios/*"
+  aws s3 rm s3://$PRODUCTION_BUCKET \
+    --recursive \
+    --exclude "android/*" \
+    --exclude "js/*" \
+    --exclude "ios/*"
   echo "Done"
 fi
 
 if [[ "$TRAVIS_BRANCH" == "$STAGING_BRANCH" ]]; then
   echo "Removing existing files on staging..."
-  aws s3 rm s3://$STAGING_BUCKET --recursive --exclude "android/*" --exclude "ios/*"
+  aws s3 rm s3://$STAGING_BUCKET \
+    --recursive \
+    --exclude "android/*" \
+    --exclude "js/*" \
+    --exclude "ios/*"
   echo "Done"
 fi
