e6cea23880e33f37f512f7be8ae712325d059772
Kwok-kuen Cheung <cheungpat@oursky.com>
2018-03-01 18:07:30 +0800

    Copy guides assets to destination

--- tasks/copy.js
+++ tasks/copy.js
@@ -2,6 +2,11 @@ const ncp = require('ncp');
 
 // Copy static files such as favicon.ico to the output (build) folder
 module.exports = () =>
-  new Promise((resolve, reject) => {
-    ncp('static', 'build', err => (err ? reject(err) : resolve()));
-  });
+  Promise.all([
+    new Promise((resolve, reject) => {
+      ncp('static', 'build', err => (err ? reject(err) : resolve()));
+    }),
+    new Promise((resolve, reject) => {
+      ncp('content/assets', 'build/assets', err => (err ? reject(err) : resolve()));
+    })
+  ]);
